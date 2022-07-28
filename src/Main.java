3bb817b441e77e2cfefa2881d2a1ce4b422aff3e
yourkeksuk <igor-18css@mail.ru>
2022-07-28 13:10:20 +0300

    Finish commit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         if (pay > 1000) {
             System.out.println("Сумма подарочных рублей: " + (discount) + "р.");
             System.out.println("Итоговая сумма счета: " + ((start) + (pay) + (discount)) + "р.");
-        }else {
+        } else {
             System.out.println("Подарочные рубли предоставляются только при пополнении на сумму более 1000р.");
             System.out.println("Итоговая сумма счета: " + ((start) + (pay)) + "р.");
         }
